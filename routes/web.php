5e84566207d00a1ca19287b93d78deaf2797cc9c
Oketa Fred <oketafred@gmail.com>
2020-10-07 23:20:54 +0300

    Added Login Regsiter and Forgot Password Routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('login');
-});
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+})->name('forgot-password');
